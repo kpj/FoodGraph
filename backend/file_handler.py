79afdbc0d697ea9d2f8196b188c4e077b42397a6
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-06-27 19:42:25 -0400

    Fixed string escaping

--- backend/file_handler.py
+++ backend/file_handler.py
@@ -2,6 +2,9 @@ class FileHandler(object):
 	def __init__(self, path):
 		self.path = path
 
+	def escape_str(self, string):
+		return string.replace(r'"', r'\"')
+
 	def save(self, data, num):
 		with open(self.path, 'w') as fd:
 			fd.write('var data = [')
@@ -10,7 +13,7 @@ class FileHandler(object):
 				for k2 in data:
 					if k2 in data[k1].keys():
 						perc = float(data[k1][k2]) / num
-						ele = '["%s", "%s", %f],\n' % (k1, k2, perc)
+						ele = '["%s", "%s", %f],\n' % (self.escape_str(k1), self.escape_str(k2), perc)
 						fd.write(ele)
 
 			fd.write(']')
