cdde645bf5abfff1b106502238e0d7e95ef5634b
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-06-28 14:00:10 -0400

    Small change

--- backend/redraw.sh
+++ backend/redraw.sh
@@ -3,7 +3,7 @@ set -u
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-find ../data -name "*.js" | while read line; do
+find "../data" -name "*.js" | while read line; do
 	png_img=${line/.js/.png}
 	echo "\"$line\" -> \"$png_img\""
 	python2 drawer.py "$line" "$png_img"
