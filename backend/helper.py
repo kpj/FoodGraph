fd7d76436edf7317e9e38bf0af5976b43602caa3
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-06-28 13:12:21 -0400

    More keywords

--- backend/helper.py
+++ backend/helper.py
@@ -1,1 +1,1 @@
-ignore_char = ['pound', 'pounds', 'tk', 'ounce', 'ounces', 'medium', 'teaspoon', 'grams', 't', 'tblsp', 'dl', 'kg', 'tbs', 'tsp', 'tbsp', 'cup', 'cups', 'l', 'g', 'oz', 'about', '/', '-', 'tablespoon', 'tablespoons', 'lb', 'lbs', 'ml', 'ts', 'tb', 'c']
+ignore_char = ['inch', 'pound', 'pounds', 'tk', 'ounce', 'ounces', 'medium', 'teaspoon', 'grams', 't', 'tblsp', 'dl', 'kg', 'tbs', 'tsp', 'tbsp', 'cup', 'cups', 'l', 'g', 'oz', 'about', '/', '-', 'tablespoon', 'tablespoons', 'lb', 'lbs', 'ml', 'ts', 'tb', 'c']
