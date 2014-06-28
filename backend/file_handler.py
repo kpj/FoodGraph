f3fb902066a434b6fc15b1681fbf9c3ed26a9180
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2014-06-27 21:55:15 -0400

    Improved file handling

--- backend/file_handler.py
+++ backend/file_handler.py
@@ -6,14 +6,18 @@ class FileHandler(object):
 		return string.replace(r'"', r'\"')
 
 	def save(self, data, num):
+		cont = 'var data = ['
+
+		for k1 in data:
+			for k2 in data:
+				if k2 in data[k1].keys():
+					perc = float(data[k1][k2]) / num
+					ele = '["%s", "%s", %f],\n' % (self.escape_str(k1), self.escape_str(k2), perc)
+					cont += ele
+
+		# remove last ',\n' to make valid json
+		cont = cont[:-2]
+
 		with open(self.path, 'w') as fd:
-			fd.write('var data = [')
-
-			for k1 in data:
-				for k2 in data:
-					if k2 in data[k1].keys():
-						perc = float(data[k1][k2]) / num
-						ele = '["%s", "%s", %f],\n' % (self.escape_str(k1), self.escape_str(k2), perc)
-						fd.write(ele)
-
+			fd.write(cont)
 			fd.write(']')
